7a19635abd92af51303e57e539a658c0a5579979
Pásztor Gábor <paszti96@gmail.com>
2020-11-01 21:31:27 +0100

    kinda cool thang

---
+++ ObjDetectionUnityEnv/Assets/scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] objects;
+    private GameObject spawnThis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+        
+    }
+
+
+    void Spawn(){
+        spawnThis = objects[Random.Range(0, objects.Length)];
+        transform.rotation = Quaternion.Euler(0, Random.Range(0,360),0);
+        Instantiate(spawnThis, gameObject.transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
