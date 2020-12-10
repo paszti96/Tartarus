ff3b31f98fcaff81275636f7e5a561b4d261e110
Pásztor Gábor <paszti96@gmail.com>
2020-12-10 22:09:53 +0100

    Glare through Bloom PostFX effect

---
+++ ObjDetectionUnityEnv/Assets/scripts/Capture.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Generate picture for the dataset
+
+public class Capture : MonoBehaviour
+{
+  void OnMouseDown(){
+      
+  }
+}
